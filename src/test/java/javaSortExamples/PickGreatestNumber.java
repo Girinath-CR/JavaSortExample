1af680416141b1fe58a8d0bee29c1a35983b7a56
Girinath CR <giri80192@gmail.com>
2022-08-22 23:41:13 +0530

    first commit

---
+++ src/test/java/javaSortExamples/PickGreatestNumber.java
@@ -0,0 +1,18 @@
+package javaSortExamples;
+
+public class PickGreatestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {10,21,45,2,66,88};
+		int greatest =arr[0];
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i]>greatest) {
+				greatest=arr[i];
+			}
+		}
+		System.out.println(greatest);
+
+	}
+
+}
